54e7dfa9c7a10e6ee6c553fa3abae8b15b2bff90
Frame_Bassman <Framebassman@gmail.com>
2016-04-18 10:27:03 +0300

    Removed redundant RegistrationPage from repo

--- PageObject/Locators.py
+++ PageObject/Locators.py
@@ -1,9 +1,5 @@
 from selenium.webdriver.common.by import By
 
-class RegistrationPageLocators(object):
-
-    SUBMIT = (By.ID, 'submitbutton')
-
 class InboxPageLocators(object):
 
     SEND_BUTTON = (By.ID, ':o4')
