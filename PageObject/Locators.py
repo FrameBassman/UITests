d535a38001d5ab1be7f68e2e6bf1ab265d40418e
Frame_Bassman <Framebassman@gmail.com>
2016-04-18 10:00:26 +0300

    Removed redundant locators

--- PageObject/Locators.py
+++ PageObject/Locators.py
@@ -17,8 +17,6 @@ class LogoutLocators(object):
     FLYOUTMENU = (By.CSS_SELECTOR, 'html.aAX body.aAU div div.nH div.nH div.nH.w-asV.aiw div.nH.oy8Mbf.qp div#gb.gb_6e.gb_T div.gb_ge.gb_9e div.gb_bb.gb_9e.gb_R.gb_8e.gb_T div.gb_cc.gb_9e.gb_R div.gb_8a.gb_wc.gb_9e.gb_R div.gb_nc.gb_ab.gb_9e.gb_R a.gb_b.gb_6a.gb_R span.gb_1a.gbii')
     SIGNOUT = (By.CSS_SELECTOR, 'html.aAX body.aAU div div.nH div.nH div.nH.w-asV.aiw div.nH.oy8Mbf.qp div#gb.gb_6e.gb_T div.gb_ge.gb_9e div.gb_bb.gb_9e.gb_R.gb_8e.gb_T div.gb_cc.gb_9e.gb_R div.gb_8a.gb_wc.gb_9e.gb_R.gb_g div.gb_eb.gb_ga.gb_g div.gb_qb div a#gb_71.gb_Da.gb_le.gb_se.gb_pb')
     ACCOUNTCHOOSER = (By.ID , 'account-chooser-link')
-    EDITACCOUNTLIST = (By.ID, 'edit-account-list')
-    ACCOUTBUTTON = (By.ID, 'choose-account-0')
     ADDACCOUNT = (By.ID, 'account-chooser-add-account')
 
 class EmailLocators(object):
